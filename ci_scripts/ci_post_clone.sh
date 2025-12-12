65f0c596a3d0a875619c0656b9910cbff42a0313
Lena Masoud <horeen@Lenas-MacBook-Air.local>
2025-12-12 17:54:10 -0500

    Hardening ci_post_clone.sh to install Flutter and dependencies in Xcode Cloud

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,7 +1,25 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
-flutter --version
+# The default execution directory of this script is the ci_scripts directory.
+# Change working directory to the root of your cloned repo.
+cd $CI_PRIMARY_REPOSITORY_PATH
+
+# 1. Install Flutter using git clone (required for CI environment)
+echo "Installing Flutter..."
+git clone https://github.com/flutter/flutter.git --depth 1 -b stable $HOME/flutter
+export PATH="$PATH:$HOME/flutter/bin"
+
+# 2. Check Flutter is working
+flutter doctor -v
+
+# 3. Install Flutter dependencies
 flutter pub get
+
+# 4. Run the build command to generate artifacts
 flutter build ios --release --no-codesign
 
+# 5. Run pod install
+cd ios
+pod install
+exit 0
