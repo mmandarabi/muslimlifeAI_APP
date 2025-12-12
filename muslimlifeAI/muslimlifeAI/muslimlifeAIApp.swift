3f8b5fa1531ad614ffa74d6eef97d7f302a4f7bc
Lena Masoud <horeen@Lenas-MacBook-Air.local>
2025-12-12 14:30:51 -0500

    Add Flutter Generated.xcconfig for Xcode Cloud

---
+++ muslimlifeAI/muslimlifeAI/muslimlifeAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  muslimlifeAIApp.swift
+//  muslimlifeAI
+//
+//  Created by Lena Masoud on 12/11/25.
+//
+
+import SwiftUI
+
+@main
+struct muslimlifeAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
